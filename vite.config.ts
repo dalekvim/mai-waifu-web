6042fcd99ceadd1c2b8de082f6e1a37586c7316a
Vimal Vinod <dalekvim@gmail.com>
2024-08-16 20:56:45 +0100

    fixed github pages error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    return {
+      plugins: [react()],
+    };
+  } else {
+    return {
+      plugins: [react()],
+      base: "https://dalekvim.github.io/mai-waifu-web/",
+    };
+  }
+});
